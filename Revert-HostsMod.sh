ad67cbd16cb1cae017e55628cc28406892eee752
Hacking MC <33645555+HackingMC@users.noreply.github.com>
2021-05-01 22:00:55 +0000

    Update Revert-HostsMod.sh

--- Revert-HostsMod.sh
+++ Revert-HostsMod.sh
@@ -4,4 +4,7 @@
   then echo "Please type "sudo ./Revert-HostsMod.sh" instead."
   exit
 fi && 
- sudo mv /etc/hosts.cache /etc/hosts
+ #Replace edited hosts file with original cache
+ sudo mv /etc/hosts.cache /etc/hosts &&
+ #wait time to create illusion of progress + tell user it is successful
+ sleep 3 && echo Successfully reverted changes.
