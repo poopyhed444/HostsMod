ca244ed85c46d7e5f2af4562446cc74be699f68d
HackingMC <33645555+hackingmc@users.noreply.github.com>
2021-05-01 21:17:01 +0000

    Update HostsMod.sh

--- HostsMod.sh
+++ HostsMod.sh
@@ -1,2 +1,14 @@
  #/bin/bash
- sudo true && cd /etc && sudo mv hosts hosts.cache && sudo wget https://raw.githubusercontent.com/hackingmc/HostsMod/main/domains && sudo cat hosts.cache domains > hosts
+ #Check for root permission
+ if [ "$EUID" -ne 0 ]
+  then echo "Please type "sudo ./HostsMod.sh" instead."
+  exit
+fi &&
+ #Temporarily move hosts file to unused file name
+ cd /etc && mv /etc/hosts /etc/hosts.cache && 
+ #download domains, updated as much as possible to include all original pushes
+ wget https://raw.githubusercontent.com/hackingmc/HostsMod/main/domains -P /etc && 
+ #Recreate hosts file with added domains on the end, redirects all domains to localhost
+ cat /etc/hosts.cache /etc/domains > /etc/hosts &&
+ #Cleanup unnecessary domains file, leaves hosts.cache in case you want to revert the changes.
+ rm /etc/domains
