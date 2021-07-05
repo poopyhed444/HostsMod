804bd9cb1a7f75d195921bd53bd74379afdb1c26
Hacking MC <33645555+HackingMC@users.noreply.github.com>
2021-07-04 22:45:38 -0500

    Reverted previous changes, did not work correctly

--- Revert-HostsMod.sh
+++ Revert-HostsMod.sh
@@ -7,10 +7,15 @@ fi
 # Fail entire script if one command fails'
 set -e
 # Find difference between old and new hosts file, to determine how many lines were added
-revertedlines=$("$(cat /etc/hosts | wc -l)"-"$(cat /etc/hosts.bak | wc -l)")
+backuplines=$(cat /etc/hosts.bak | wc -l)
+currentlines=$(cat /etc/hosts | wc -l)
+revertlines=$((currentlines-backuplines))
+# Remove unnecessary variables
+unset backuplines
+unset currentlines
 # Replace edited hosts file with the backup
 mv /etc/hosts.bak /etc/hosts
 # Tell user it is successful
-echo Successfully removed $revertedlines domains from the hosts file
-# Remove variable
-unset revertedlines
+echo Successfully removed $revertlines domains from the hosts file
+# Remove last variable
+unset revertlines
