6957377b6e843c0c4238b763584cb156d01e5a85
Hacking MC <33645555+HackingMC@users.noreply.github.com>
2021-07-21 20:50:42 -0700

    Test

--- HostsMod.sh
+++ HostsMod.sh
@@ -4,18 +4,15 @@ if [ "$EUID" -ne 0 ]; then
   echo "Please prepend $(tput setaf 3)\"sudo\"$(tput sgr0) to this command."
   exit 1
 fi
-# Check if backup file already exists
-if [ ! -f /etc/hosts.bak ]; then
-  echo "Backup hosts file found, it is recommended you run the revert script instead."
-fi
-# If backup file is not found, proceed
-else
 # Fail entire script if one command fails'
 set -e
 # Back up the old hosts file
 cp /etc/hosts /etc/hosts.bak
-# Append the remote domains file to the /etc/hosts file
-curl -sL https://raw.githubusercontent.com/HackingMC/HostsMod/main/domains >> /etc/hosts
+# Download the remote domains file
+curl -sL https://github.com/GardeningTool/HostsMod/raw/main/domains.txt > /etc/domains.txt
+# Add localhost onto the domains, append to hosts file
+awk '{print "127.0.0.1 "$0}' /etc/domains.txt >> /etc/hosts
 # Notify user the script has successfully added domains, using lines variable
-echo "Successfully added "$(curl -sL https://raw.githubusercontent.com/HackingMC/HostsMod/main/domains | cat | wc -l)" domains to hosts file."
-fi
+echo "Successfully added "$(cat /etc/domains.txt | wc -l)" domains to hosts file."
+# Clean Up
+rm /etc/domains.txt
